e8c2cc04356d9ec5606b6ffb3cf6512432409e4b
BEagle1984 <sergio@aquilini.ch>
2024-08-01 18:32:13 +0200

    feat: implement Protobuf serializer

---
+++ src/Silverback.Integration.Kafka.SchemaRegistry/Messaging/Configuration/ProtobufMessageTypeValidator.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2024 Sergio Aquilini
+// This code is licensed under MIT license (see LICENSE file for details)
+
+using System;
+using Google.Protobuf;
+
+namespace Silverback.Messaging.Configuration;
+
+internal static class ProtobufMessageTypeValidator
+{
+    public static void Validate(Type messageType)
+    {
+        if (!typeof(IMessage).IsAssignableFrom(messageType))
+            throw new SilverbackConfigurationException($"{messageType.Name} does not implement IMessage<{messageType.Name}>.");
+
+        Type iMessageType = typeof(IMessage<>).MakeGenericType(messageType);
+        if (!iMessageType.IsAssignableFrom(messageType))
+            throw new SilverbackConfigurationException($"{messageType.Name} does not implement IMessage<{messageType.Name}>.");
+
+        if (messageType.GetConstructor(Type.EmptyTypes) == null)
+            throw new SilverbackConfigurationException($"{messageType.Name} does not have a public parameterless constructor.");
+    }
+}
